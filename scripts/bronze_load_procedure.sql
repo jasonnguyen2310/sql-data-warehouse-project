a0d2c4ba09b423ddc09ed2ad3ef8cba3975f59ea
jasonnguyen2310 <153895856+jasonnguyen2310@users.noreply.github.com>
2025-03-13 16:38:53 +0700

    Update and rename initial_load.sql to bronze_load_procedure.sql

---
+++ scripts/bronze_load_procedure.sql
@@ -0,0 +1,39 @@
+// Load data from csv to postgres
+
+CREATE OR REPLACE PROCEDURE bronze.load_bronze()
+LANGUAGE plpgsql
+AS $$
+BEGIN
+
+	TRUNCATE bronze.crm_cust_info;
+	COPY bronze.crm_cust_info
+	FROM 'C:\DE Project\sql-data-warehouse-project\datasets\source_crm\cust_info.csv' 
+	WITH (FORMAT CSV, HEADER true);
+	
+	TRUNCATE bronze.crm_prd_info;
+	COPY bronze.crm_prd_info
+	FROM 'C:\DE Project\sql-data-warehouse-project\datasets\source_crm\prd_info.csv' 
+	WITH (FORMAT CSV, HEADER true);
+	
+	TRUNCATE bronze.crm_sales_details;
+	COPY bronze.crm_sales_details
+	FROM 'C:\DE Project\sql-data-warehouse-project\datasets\source_crm\sales_details.csv' 
+	WITH (FORMAT CSV, HEADER true);
+	
+	TRUNCATE bronze.erp_CUST_AZ12;
+	COPY bronze.erp_CUST_AZ12
+	FROM 'C:\DE Project\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv' 
+	WITH (FORMAT CSV, HEADER true);
+	
+	TRUNCATE bronze.erp_LOC_A101;
+	COPY bronze.erp_LOC_A101
+	FROM 'C:\DE Project\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv' 
+	WITH (FORMAT CSV, HEADER true);
+	
+	TRUNCATE bronze.erp_PX_CAT_G1V2;
+	COPY bronze.erp_PX_CAT_G1V2
+	FROM 'C:\DE Project\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv' 
+	WITH (FORMAT CSV, HEADER true);
+
+END;
+$$;
